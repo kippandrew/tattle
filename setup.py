eb241ef231d34d970ccecca1f1bbfae5137ed050
Andy Kipp <andy@rstudio.com>
2017-01-18 16:26:08 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='tattle',
+      version='0.1',
+      description='Gossip Protocol for Python 3.5+',
+      author='Andy Kipp',
+      author_email='kipp.andrew@gmail.com',
+      packages=['tattle'])
