eac7bec4e7f414a7a1af197b342b2ebd03e3a98d
Andy Kipp <andy@rstudio.com>
2017-03-24 15:53:43 -0700

    Setup travis CI

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.5
 from distutils.core import setup
 
 setup(name='tattle',
       version='0.1',
       description='Gossip Protocol for Python 3.5+',
+      url='https://github.com/kippandrew/tattle',
       author='Andy Kipp',
       author_email='kipp.andrew@gmail.com',
       packages=['tattle'])
